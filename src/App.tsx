4ec4ceea10daec5f72d3c42a51cad296555655ae
wjdghks963 <74060017+wjdghks963@users.noreply.github.com>
2021-05-17 23:08:14 +0900

    props

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import styled from "styled-components";
+import React, { Component } from 'react';
+import Number from "./Number";
 
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IState {
+  counter:number;  
 }
+
+class App extends Component<{}, IState> {
+  state ={
+    counter:0
+  };
+  render() {
+    const {counter} = this.state;
+   return <div><Number count={counter}/><button onClick={this.add}></button></div>;
+  }
+  add = () => {
+    this.setState(prev =>{
+      return {counter: prev.counter+1
+      };
+    });
+  };
+}
+
 
 export default App;
